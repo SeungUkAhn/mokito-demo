749e7cf1ea9db72d314f0bf09ff1715a93ed3823
SeungUk Ahn <ahn2802@gmail.com>
2023-08-23 04:36:40 +0900

    Mokito 프로젝트 생성

---
+++ src/test/java/com/maicoding/mokitodemo/mokitodemo/MokitoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maicoding.mokitodemo.mokitodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MokitoDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
