fd5e44a35563ca0a56e68e5a7b0d5737b3df3c01
SeungUk Ahn <ahn2802@gmail.com>
2023-08-23 05:14:52 +0900

    @Mock, @InjectMocks, @ExtendWith() 어노테이션을 활용한 코드 리팩토링

--- src/test/java/com/maicoding/mokito/mokitodemo/business/SomeBusinessImplMockTest.java
+++ src/test/java/com/maicoding/mokito/mokitodemo/business/SomeBusinessImplMockTest.java
@@ -1,29 +1,40 @@
 package com.maicoding.mokito.mokitodemo.business;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class SomeBusinessImplMockTest {
+
+    @Mock
+    private DataService dataServiceMock;
+
+    @InjectMocks
+    private SomeBusinessImpl someBusiness;
 
     @Test
     void findTheGreatestFromALlData_basicScenario(){
-        DataService dataServiceMock = mock(DataService.class);
         when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {25, 15, 5});
-        SomeBusinessImpl someBusiness = new SomeBusinessImpl(dataServiceMock);
-        int result = someBusiness.findTheGreatestFromALlData();
-        assertEquals(25, result);
+        assertEquals(25, someBusiness.findTheGreatestFromALlData());
     }
 
 
     @Test
     void findTheGreatestFromALlData_OneValue(){
-        DataService dataServiceMock = mock(DataService.class);
         when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {35});
-        SomeBusinessImpl someBusiness = new SomeBusinessImpl(dataServiceMock);
-        int result = someBusiness.findTheGreatestFromALlData();
-        assertEquals(35, result);
+        assertEquals(35, someBusiness.findTheGreatestFromALlData());
+    }
+
+    @Test
+    void findTheGreatestFromALlData_EmptyArray(){
+        when(dataServiceMock.retrieveAllData()).thenReturn(new int[] {});
+        assertEquals(Integer.MIN_VALUE, someBusiness.findTheGreatestFromALlData());
     }
 }
